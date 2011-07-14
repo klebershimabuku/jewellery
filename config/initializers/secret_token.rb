467c57d08a91db734d2d6645808d5b6f6f97147c
Kleber Shimabuku <klebershimabuku@iMac-de-Kleber-Shimabuku.local>
2011-07-14 23:17:52 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jewellery::Application.config.secret_token = '6d832080c6ab99c97e3a087bca26cedcabad60127d6a30a38cdb21e375409af738eebde24972e35a8b2825ab3fa26c979d03591556313d9f3b68f878ac801f52'
