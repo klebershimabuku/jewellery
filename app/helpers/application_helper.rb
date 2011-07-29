39bd95a14f9ddfc05d092d9ac5fd4a51835184b8
Kleber Shimabuku <klebershimabuku@gmail.com>
2011-07-29 13:20:49 +0900

    added css3buttons stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,5 +31,9 @@ module ApplicationHelper
       value
     end
   end
+  
+  def button_for(name, options={})
+      return content_tag(:button, content_tag(:span, content_tag(:span, name)), :class => options[:class], :type => options[:button_type])
+  end
     
 end
