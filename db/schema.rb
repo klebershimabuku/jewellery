77de09855f4bc59c5f941ee2652526c28eeb7444
Kleber Shimabuku <klebershimabuku@gmail.com>
2011-08-14 15:43:56 +0900

    upload module done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110811113854) do
+ActiveRecord::Schema.define(:version => 20110813212914) do
 
   create_table "item_parts", :force => true do |t|
     t.integer  "item_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20110811113854) do
     t.integer  "item_type_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo"
   end
 
   create_table "part_types", :force => true do |t|
